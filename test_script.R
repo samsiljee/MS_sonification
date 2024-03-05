7363118cf279a43c13ec018342f03558fe3ae7eb
samsiljee <samsiljee@gmail.com>
2024-03-06 10:08:40 +1300

    Added a few more example tones, and code to load test spectra from .csv files

--- test_script.R
+++ test_script.R
@@ -13,17 +13,26 @@ source("tonify_spectrum.R")
 # Open test MS file
 ms <- openMSfile("22-091_1_1ul_SS.mzML")
 
-# Extract the 700th spectrum
-spectrum <- as.data.frame(peaks(ms, 311))
-spectrum2 <- as.data.frame(peaks(ms, 401))
+# Extract some spectra
+spectrum_1 <- as.data.frame(peaks(ms, 2311))
+spectrum_2 <- as.data.frame(peaks(ms, 8401))
+
+# Load test spectrum
+spectrum_3 <- read.csv("test_spectra/spectrum_1.csv", header = TRUE)
+spectrum_4 <- read.csv("test_spectra/spectrum_2.csv", header = TRUE)
 
 # Run the function
 # audio_tone <- tonify_spectrum(spectrum)
-audio_tone2 <- tonify_spectrum(spectrum)
+audio_tone_1 <- tonify_spectrum(spectrum_1)
+audio_tone_2 <- tonify_spectrum(spectrum_2)
+audio_tone_3 <- tonify_spectrum(spectrum_4)
+audio_tone_3 <- tonify_spectrum(spectrum_4)
 
 # Play the sound
-play(audio_tone)
-play(audio_tone2)
+play(audio_tone_1)
+play(audio_tone_2)
+play(audio_tone_3)
+play(audio_tone_4)
 
 # Save the audio as a WAV file
-writeWave(audio_obj, file = "polyphonic_sound.wav")
+writeWave(audio_tone_1, file = "polyphonic_sound.wav")

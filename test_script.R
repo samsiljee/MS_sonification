d3761ebbe9d87a101c7b504edc5c149051b9374c
samsiljee <samsiljee@gmail.com>
2024-03-06 07:39:12 +1300

    Now working with test data

--- test_script.R
+++ test_script.R
@@ -4,21 +4,26 @@
 
 # Load the required packages
 library(tuneR)
+library(mzR)
+library(dplyr)
 
-# Source the function
+# Source the tonification function
 source("tonify_spectrum.R")
 
-# test data
-spectrum <- data.frame(
-  frequency = c(440, 660, 880),
-  amplitude = c(0.1, 0.5, 0.5)
-)
+# Open test MS file
+ms <- openMSfile("22-091_1_1ul_SS.mzML")
+
+# Extract the 700th spectrum
+spectrum <- as.data.frame(peaks(ms, 311))
+spectrum2 <- as.data.frame(peaks(ms, 401))
 
 # Run the function
-audio_obj <- tonify_spectrum(spectrum)
+# audio_tone <- tonify_spectrum(spectrum)
+audio_tone2 <- tonify_spectrum(spectrum)
 
 # Play the sound
-play(audio_obj)
+play(audio_tone)
+play(audio_tone2)
 
 # Save the audio as a WAV file
 writeWave(audio_obj, file = "polyphonic_sound.wav")

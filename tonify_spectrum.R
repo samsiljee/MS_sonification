d7982c314e0827b978f66562a0dd7acae08a7775
samsiljee <samsiljee@gmail.com>
2024-03-06 07:38:57 +1300

    Fixed to work with spectra from test data

--- tonify_spectrum.R
+++ tonify_spectrum.R
@@ -3,9 +3,16 @@
 # Created 5th March 2024
 # Input: A table input of two columns, one column (m/z of the peak) determines the frequency of the sinewave, the second (intensity) determines the relative amplitude of that peak in the polyphonic signal
 # Output: An audio object of one second duration
-# This function requires the "tuneR" package
+# This function requires the "tuneR" and "dplyr" packages
 
 tonify_spectrum <- function(spectrum) {
+  # Filter out peaks of 0 intensity
+  dat <- spectrum %>%
+    filter(intensity != 0)
+  
+  # Normalise the intensity
+  dat$intensity <- dat$intensity / max(dat$intensity) * 10000000
+  
   # Create a blank time vector for 1s of tone
   time <- seq(0, 2 * pi, length = 44100)
 
@@ -13,9 +20,9 @@ tonify_spectrum <- function(spectrum) {
   sound_signal <- numeric(length(time))
 
   # Add a sinewave for each row of the table
-  for (i in 1:nrow(spectrum)) {
+  for (i in 1:nrow(dat)) {
     # Generate sine wave
-    sine_wave <- round(spectrum$amplitude[i] * sin(spectrum$frequency[i] * time))
+    sine_wave <- dat$intensity[i] * sin(round(dat$mz[i]) * time)
 
     # Add the sine wave to the sound signal
     sound_signal <- sound_signal + sine_wave
@@ -25,5 +32,5 @@ tonify_spectrum <- function(spectrum) {
   sound_signal <- (sound_signal / max(abs(sound_signal))) * 32000
 
   # Create an audio object
-  return(Wave(sound_signal, samp.rate = 44100, bit = 16))
+  return(Wave(round(sound_signal), samp.rate = 44100, bit = 16))
 }

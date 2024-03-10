de6c6337b891dd3906a4a71e27dff8b1fb01e0c2
samsiljee <samsiljee@gmail.com>
2024-03-11 07:42:54 +1300

    now writing and reading waveforms to .txt properly

--- sonify_run.R
+++ sonify_run.R
@@ -15,23 +15,23 @@ source("functions.R")
 # Open test MS file
 ms_data <- openMSfile("22-091_1_1ul_SS.mzML")
 
-# Extract all the peaks
-ms_peaks <- peaks(ms_data)
+# Extract the first 10,000 the peaks
+ms_peaks <- peaks(ms_data, scans = 1:10)
 
-# Extract correspondign header information
-ms_header <- header(ms_data)
+# Extract corresponding header information
+ms_header <- header(ms_data) %>% head(10)
 
 # Get list of MS1 and MS2 spectrum indexes
 ms_1_indexes <- filter(ms_header, msLevel == 1) %>% .$seqNum
 ms_2_indexes <- filter(ms_header, msLevel == 2) %>% .$seqNum
 
 # Inverse Fourier transform to get waveforms, and write to folder
-for(i in nrow(ms_header)) {
+for (i in 1:nrow(ms_header)) {
   # Reverse Fourier transform
-  waveform <- reversefouriertransform(peaks[i])
-    
+  waveform <- spectrum_to_waveform(as.data.frame(ms_peaks[[i]]))
+
   # Write to .txt file to interrupt long tasks
-  write(waveform, file = paste0("waveforms/waveform_", i, ".txt"))
+  write(waveform, ncolumns = 1, file = paste0("waveforms/waveform_", i, ".txt"))
 }
 
 # Initialise blank matrix for all waveforms
@@ -41,26 +41,30 @@ waveform_matrix <- matrix()
 total_time <- 44100 * max(ms_header$retentionTime)
 
 # Read in the .txt files
-for(i in list.files("waveforms")) {
-  # Generate blank time before and after the waveform
-  RT_pre <- rep(0, 44100 * ms_header[i]$retentionTime)
-  RT_post <- rep(0, total_time - RT_pre)
-  
-  # read in the waveform and account for intensity
-  waveform <- read(paste0("waveforms/", i)) * ms_header[i]$totIonCurrent
-  
-  # Add in blank time before and after
-  waveform <- c(RT_pre, waveform, RT_post)
-  
-  # Add to the waveform matrix
-  waveform_matrix <- rbind(waveform_matrix, waveform)
+for (i in list.files("waveforms")) {
+  ## Generate blank time before and after the waveform
+  # RT_pre <- rep(0, 44100 * ms_header[i]$retentionTime)
+  # RT_post <- rep(0, total_time - RT_pre)
+
+  # read in the waveform
+  waveform <- read.delim(paste0("waveforms/", i), header = FALSE)[, 1]
+  print(length(waveform))
+
+  ## Account for intensity
+  # waveform <- waveform * ms_header[i]$totIonCurrent
+  # 
+  # # Add in blank time before and after
+  # waveform <- c(RT_pre, waveform, RT_post)
+
+  # # Add to the waveform matrix
+  # waveform_matrix <- rbind(waveform_matrix, waveform)
 }
 
 # Add all of the waveforms together
 run_waveform <- colSums(waveform_matrix)
 
 # Normalise
-run_waveform <- (run_waveform/max(abs(run_waveform))) * 32000
+run_waveform <- (run_waveform / max(abs(run_waveform))) * 32000
 
 # Save as .wav
 writeWave(run_waveform, file = "run.wav")

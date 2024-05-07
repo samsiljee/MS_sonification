34b0230e584cf6807b7b5044f261d0c440bb943c
samsiljee <samsiljee@gmail.com>
2024-05-07 13:52:48 +1200

    Fixed website not loading by defining the function in the script

--- Tonification_shiny/app.R
+++ Tonification_shiny/app.R
@@ -7,8 +7,66 @@ library(shiny)
 library(tuneR)
 library(dplyr)
 
-# Source functions
-source("../functions.R", local = TRUE)
+# Define functions
+# Generate the function to test different options
+advanced_spectrum_to_tone <- function(
+    spectrum,
+    duration = 1,
+    sampling_rate = 44100,
+    filter_mz = FALSE,
+    filter_threshold = 0.5,
+    scale = FALSE,
+    scale_min = 100,
+    scale_max = 15000,
+    log_transform = FALSE,
+    reverse_mz = FALSE) {
+  # Filter out peaks of 0 intensity
+  dat <- as.data.frame(spectrum) %>%
+    filter(intensity > 0)
+  
+  # Filter more peaks if selected
+  if (filter_mz) {
+    # Arrange the data
+    dat <- arrange(dat, by = desc(mz))
+    # Take the top X peaks
+    dat <- dat[1:round(nrow(dat) * filter_threshold), ]
+  }
+  
+  # Log2 transform if selected
+  if (log_transform) {
+    # Save old m/z values for re-scaling if needed
+    old_min <- min(dat$mz)
+    old_max <- max(dat$mz)
+    # Log transform
+    dat$mz <- log2(dat$mz)
+    # Re-scale if not scaling separately
+    if(!scale) {
+      dat$mz <- (dat$mz - min(dat$mz)) / max(dat$mz - min(dat$mz)) * (old_max - old_min) + old_min
+    }
+  }
+  
+  # Scale m/z if selected
+  if (scale) {
+    dat$mz <- (dat$mz - min(dat$mz)) / max(dat$mz - min(dat$mz)) * (scale_max - scale_min) + scale_min
+  }
+  
+  # Reverse mz values if selected
+  if (reverse_mz) {
+    dat$mz <- ((dat$mz - mean(c(max(dat$mz), min(dat$mz)))) * -1) + mean(c(max(dat$mz), min(dat$mz)))
+  }
+  
+  # Create time sequence
+  time_seq <- seq(0, duration * 2 * pi, length = duration * sampling_rate)
+  
+  # Create and add a sine wave for every peak
+  sound_signal <- (sin(outer(time_seq, dat$mz, "*")) %*% dat$intensity)
+  
+  # Normalize the sound signal and return as numeric vector
+  sound_signal <- round((sound_signal / max(abs(sound_signal))) * 32000)
+  
+  # Return audio object
+  return(Wave(round(sound_signal), samp.rate = 44100, bit = 16))
+}
 
 # Define UI
 ui <- fluidPage(
@@ -40,7 +98,7 @@ ui <- fluidPage(
       value = 15000)),
   checkboxInput("log_transform", "Log transform", value = FALSE),
   checkboxInput("reverse_mz", "Reverse m/z values"),
-  actionButton("tonify", "Tonify!"),
+  actionButton("tonify", "Generate tone!"),
   downloadButton("download_wav", "Download .wav file"),
   verbatimTextOutput("test")
 )

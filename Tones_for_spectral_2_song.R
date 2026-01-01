847f62066b39890122d8147711a2ca54db0e45ef
samsiljee <samsiljee@gmail.com>
2026-01-02 11:44:52 +1300

    Updated to match new parameters for advanced spectrum to tone. also increased duration to 7 minutes

--- Tones_for_spectral_2_song.R
+++ Tones_for_spectral_2_song.R
@@ -22,14 +22,14 @@ ms_header <- header(ms_data)
 # Extract spectrum with the most peaks
 top_spectrum <- peaks(ms_data, which(ms_header$peaksCount == max(ms_header$peaksCount)))
 
-# Generate tones
 tone <- advanced_spectrum_to_tone(
   spectrum = top_spectrum,
-  duration = 5,
+  duration = (7*60),
   scale = TRUE,
   scale_min = 100,
-  scale_max = 15000
+  scale_max = 15000,
+  waveform = "sine"
 )
 
 # Save as audio clip
-writeWave(tone, file = "tones/wide_drone.wav")
+writeWave(tone, file = "tones/triangle.wav")
